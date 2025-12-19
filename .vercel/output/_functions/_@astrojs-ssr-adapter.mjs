e834fc2583f4e7593618b59c019620047bb2ed7b
srwebcl <contacto@srweb.cl>
2025-12-19 12:18:19 -0300

    fix: remove tracked .vercel folder and add to gitignore

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter_CEa5Ng9E.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_DoE-0Ov1.mjs';
-export { renderers } from '../renderers.mjs';
